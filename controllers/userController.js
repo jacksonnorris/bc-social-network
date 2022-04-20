0f62c5a2919956d99f34ccd0d72e25b4edd4d726
jacksonnorris <jacksonnorr@gmail.com>
2022-04-19 22:29:00 -0400

    added ability to get user by id and delete user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,5 +15,39 @@ module.exports = {
     User.create(req.body)
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.id })
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.id })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found" })
+          : User.findOneAndUpdate(
+            {
+              users: req.params.id
+            },
+            {
+              $pull: { users: req.params.id }
+            },
+            {
+              new: true
+            },
+            res.json({ message: 'User deleted' })
+          )
+
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   }
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,10 +2,15 @@ const router = require('express').Router();
 const {
   // insert added methods
   getUsers,
-  createUser
+  createUser,
+  getSingleUser,
+  deleteUser
 } = require('../../controllers/userController.js');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:id
+router.route('/:id').get(getSingleUser).delete(deleteUser);
 
 module.exports = router;
